92c7d6c05981c2ace8afc89bda1c779074e5d6aa
Gleb Dianov <gleb.dianov@protonmail.com>
2021-10-02 19:18:50 +0400

    write some smart contract tests

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,31 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const TodoList = artifacts.require("./TodoList.sol");
 
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+contract("TodoList", accounts => {
+    before(async () => {
+        this.todoList = await TodoList.deployed();
+    });
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    it('deploys successfully', async () => {
+        const address = await this.todoList.address;
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, '');
+        assert.notEqual(address, null);
+        assert.notEqual(address, undefined);
+    });
 
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
+    it('gets the right task by task id', async () => {
+        const taskCount = await this.todoList.taskCount();
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
-  });
+        const task = await this.todoList.tasks(taskCount);
+
+        assert.equal(task.id.toNumber(), taskCount.toNumber());
+    });
+
+    it('can get the first task', async () => {
+        const task = await this.todoList.tasks(1);
+
+        assert.equal(task.id.toNumber(), 1);
+        assert.equal(task.content, 'Write your first Ethereum smart contract');
+        assert.equal(task.completed, false);
+    });
 });
