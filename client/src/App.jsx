a8887d1fd0f598146485efee9c72aaeef15079a3
Gleb Dianov <gleb.dianov@protonmail.com>
2021-10-03 18:33:34 +0400

    implement FE task creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,72 +5,96 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { taskCount: 0, web3: null, accounts: null, contract: null, tasks: [] };
+    state = {
+        taskCount: 0,
+        web3: null,
+        accounts: null,
+        contract: null,
+        tasks: [],
+        newTaskContent: ""
+    };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+    componentDidMount = async () => {
+        try {
+            // Get network provider and web3 instance.
+            const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+            // Use web3 to get the user's accounts.
+            const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = TodoListContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        TodoListContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+            // Get the contract instance.
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = TodoListContract.networks[networkId];
+            const instance = new web3.eth.Contract(
+                TodoListContract.abi,
+                deployedNetwork && deployedNetwork.address,
+            );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
+            // Set web3, accounts, and contract to the state, and then proceed with an
+            // example of interacting with the contract's methods.
+            this.setState({ web3, accounts, contract: instance }, this.runExample);
+        } catch (error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to load web3, accounts, or contract. Check console for details.`,
+            );
+            console.error(error);
+        }
+    };
 
-  runExample = async () => {
-    const { taskCount, contract } = this.state;
+    runExample = async () => {
+        const { tasks, taskCount, contract } = this.state;
 
-    // await contract.methods.createTask("Figure out Web3 FE").send({ from: accounts[0] });
+        // Get the value from the contract to prove it worked.
+        const newTaskCount = await contract.methods.taskCount().call();
 
-    // Get the value from the contract to prove it worked.
-    const newTaskCount = await contract.methods.taskCount().call();
+        console.log(taskCount);
+        console.log(newTaskCount);
 
-    let tasks = [];
+        for (let i = taskCount + 1; i <= newTaskCount; i++) {
+            const newTask = await contract.methods.tasks(i).call();
+            tasks.push(newTask);
+        }
 
-    for (let i = taskCount + 1; i <= newTaskCount; i++) {
-        const newTask = await contract.methods.tasks(i).call();
-        tasks.push(newTask);
-    }
+        console.log(tasks);
 
-    // Update state with the result.
-    this.setState({ taskCount: newTaskCount, tasks });
-  };
+        // Update state with the result.
+        this.setState({ taskCount: newTaskCount, tasks });
+    };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    newTaskChanged = event => this.setState({ newTaskContent: event.target.value });
 
-    console.log(this.state.tasks);
+    createNewTask = async event => {
+        event.preventDefault();
 
-    return (
-      <div className="App">
-        <h1>Ethereum Todo List</h1>
-        <div>Total number of tasks: {this.state.taskCount}</div>
-        <h2>Todos:</h2>
-        <ul>
-          {this.state.tasks.map(task => <li key={task.id}>{task.content}</li>)}
-        </ul>
-      </div>
-    );
+        const { contract, accounts, tasks } = this.state;
+
+        const response = await contract.methods.createTask(this.state.newTaskContent).send({ from: accounts[0] });
+
+        tasks.push(response.events.TaskCreated.returnValues);
+
+        this.setState({ newTaskContent: '', tasks });
+    };
+
+    render() {
+        if (!this.state.web3) {
+            return <div>Loading Web3, accounts, and contract...</div>;
+        }
+
+        return (
+        <div className="App">
+            <h1>Ethereum Todo List</h1>
+            <div>Total number of tasks: {this.state.taskCount}</div>
+            <form onSubmit={this.createNewTask}>
+                <input type="text" onChange={this.newTaskChanged} value={this.state.newTaskContent}/>
+                <input type="submit" hidden="" />
+            </form>
+            <h2>Todos:</h2>
+            <ul>
+            {this.state.tasks.map(task => <li key={task.id}>{task.content}</li>)}
+            </ul>
+        </div>
+        );
   }
 }
 
